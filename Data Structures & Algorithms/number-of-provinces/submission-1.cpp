d4f808fac069f286d6545d9a30b89d9bfd9a86ca
Parag Jain <142827074+Parag1609@users.noreply.github.com>
2026-06-16 01:30:19 +0530

    Add: number-of-provinces - submission-1

---
+++ Data Structures & Algorithms/number-of-provinces/submission-1.cpp
@@ -0,0 +1,27 @@
+class Solution {
+    int n;
+public:
+    void dfs(vector<vector<int>>& isConnected,int u,vector<bool>& visited){
+        visited[u]=true;
+
+        for(int v=0;v<n;v++){
+            if(!visited[v] && isConnected[u][v]){
+                dfs(isConnected,v,visited);
+            }
+        }
+    }
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        n=isConnected.size();
+        vector<bool>visited(n,false);
+
+        
+        int count = 0;
+        for(int i=0;i<n;i++){
+            if(!visited[i]){
+                dfs(isConnected,i,visited);
+                count++;
+            }
+        }
+        return count;
+    }
+};
