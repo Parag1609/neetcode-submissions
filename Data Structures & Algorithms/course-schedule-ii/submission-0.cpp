ad41f72aa4a0bc3f6f898c41f5e1bb8d9ad71864
Parag Jain <142827074+Parag1609@users.noreply.github.com>
2026-06-20 00:39:01 +0530

    Add: course-schedule-ii - submission-0

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-0.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+         vector<bool>visited(numCourses,false);
+        vector<int>indegree(numCourses,0);
+        vector<vector<int>>adj(numCourses);
+        queue<int>q;
+        vector<int>res;
+        for(auto a:prerequisites){
+            int prereq=a[1];
+            int course=a[0];
+            adj[prereq].push_back(course);
+            indegree[course]++;
+        }
+        int count=0;
+        for(int i=0;i<numCourses;i++){
+            if(indegree[i]==0){
+                q.push(i);
+            }
+        }
+        while(!q.empty()){
+            int node=q.front();
+            q.pop();
+            count ++;
+            res.push_back(node);
+            for(int &v:adj[node]){
+                indegree[v]--;
+                if(indegree[v]==0)q.push(v);
+            }
+        }
+        return count==numCourses? res: vector<int>{};      
+    }
+};
